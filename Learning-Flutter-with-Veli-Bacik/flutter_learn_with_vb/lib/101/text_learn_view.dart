9933fa925145d5c0c7dc105831a14180e1a292db
Baha Sorkun <114817265+bahasorkun@users.noreply.github.com>
2025-01-15 17:01:48 +0300

    Kod düzeni öğreniliyor

--- Learning-Flutter-with-Veli-Bacik/flutter_learn_with_vb/lib/101/text_learn_view.dart
+++ Learning-Flutter-with-Veli-Bacik/flutter_learn_with_vb/lib/101/text_learn_view.dart
@@ -1,18 +1,60 @@
 import 'package:flutter/material.dart';
 
 class TextLearnView extends StatelessWidget {
-  const TextLearnView({super.key});
+  TextLearnView({super.key});
 
+  final String name = "Baha";
+  final ProjectKeys keys = ProjectKeys();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Text(
-          ("Buy the best one" *
-              10), //Dart dili burayı otomatik olarak bu yazıyı 10 kez tekrar edecek şekilde algılar.
-              
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text("Welcome $name ${name.length}",
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+                style: ProjectStyles.welcomeStyle),
+            Text("Hello $name ${name.length}",
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,
+                textAlign: TextAlign.center,
+                style: ProjectStyles.welcomeStyle),
+            Text(
+              "Merhaba $name ${name.length}",
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                  color: ProjectColors.welcomeColor,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(keys.welcome)
+          ],
         ),
       ),
     );
   }
+}
+
+//Kod Okunabilirliği ve Düzeni İçin bir örnek
+class ProjectStyles {
+  static TextStyle welcomeStyle = const TextStyle(
+      wordSpacing: 2,
+      decoration: TextDecoration.underline,
+      fontStyle: FontStyle.italic,
+      letterSpacing: 2,
+      color: Colors.black,
+      fontSize: 16,
+      fontWeight: FontWeight.bold);
+}
+
+class ProjectColors {
+  static Color welcomeColor = Colors.red;
+}
+
+class ProjectKeys {
+  final String welcome = "Merhaba";
 }
