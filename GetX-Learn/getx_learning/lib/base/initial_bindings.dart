d61b2fdbad7acb5d65a139f503dcce13deef7121
Baha Sorkun <114817265+bahasorkun@users.noreply.github.com>
2025-03-07 13:51:51 +0300

    Update initial_bindings.dart

--- GetX-Learn/getx_learning/lib/base/initial_bindings.dart
+++ GetX-Learn/getx_learning/lib/base/initial_bindings.dart
@@ -4,7 +4,6 @@ import 'package:getx_learning/base/test_controller.dart';
 class InitialBinding implements Bindings {
   @override
   void dependencies() {
-    
     Get.put(TestController());
   }
 }
