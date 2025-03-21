1aeabef39ecb869dba71e3a1a84a9b320f55caea
Baha Sorkun <114817265+bahasorkun@users.noreply.github.com>
2025-03-21 13:46:09 +0300

    firebase auth kurulumu

---
+++ firebaseAuth/firebase_auth_app/lib/features/auth/repository/auth_repo.dart
@@ -0,0 +1,20 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:riverpod/riverpod.dart';
+
+final authRepositoryProvider = Provider<AuthRepository>((ref) {
+  final auth = FirebaseAuth.instance;
+  return AuthRepository(auth: auth);
+});
+
+class AuthRepository {
+  final FirebaseAuth auth;
+  AuthRepository({required this.auth});
+
+  Future<void> signInWithEmailAndPassword({
+    required String email,
+    required String password,
+  }) async {
+    await auth.signInWithEmailAndPassword(email: email, password: password);
+  }
+}
