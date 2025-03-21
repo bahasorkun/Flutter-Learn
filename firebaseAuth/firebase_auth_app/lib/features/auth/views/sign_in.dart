9e7a60836c109fc6bd8f4462c4d6a769611ac596
Baha Sorkun <114817265+bahasorkun@users.noreply.github.com>
2025-03-21 13:29:47 +0300

    sigin in ui done

--- firebaseAuth/firebase_auth_app/lib/features/auth/views/sign_in.dart
+++ firebaseAuth/firebase_auth_app/lib/features/auth/views/sign_in.dart
@@ -26,7 +26,7 @@ class _SignInState extends State<SignIn> {
             ),
           ),
           AspectRatio(
-            aspectRatio: 1,
+            aspectRatio: 0.95,
             child: Container(
               padding: EdgeInsets.all(15),
               decoration: BoxDecoration(
@@ -46,13 +46,79 @@ class _SignInState extends State<SignIn> {
                       style: TextStyle(color: titleColor, fontSize: 24),
                     ),
                   ),
-                  TextFormField(
-                    decoration: InputDecoration(
-                      labelText: "Email",
-                      labelStyle: TextStyle(color: titleColor),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        labelStyle: TextStyle(color: titleColor),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: borderColor),
+                        ),
                       ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 5),
+                    child: TextFormField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        labelText: "Password",
+                        labelStyle: TextStyle(color: titleColor),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: borderColor),
+                        ),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  MaterialButton(
+                    onPressed: () {},
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(4),
+                    ),
+                    color: buttonColor,
+                    minWidth: double.infinity,
+                    height: 40,
+                    child: Text(
+                      "Sign In",
+                      style: TextStyle(color: containerColor),
+                    ),
+                  ),
+                  Container(
+                    alignment: Alignment.centerLeft,
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    child: InkWell(
+                      onTap: () {},
+                      child: Text(
+                        "Forgot Password ?",
+                        style: TextStyle(color: textButtonColor, fontSize: 14),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 3),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Don't have an account ?",
+                          style: TextStyle(
+                            color: textButtonColor,
+                            fontSize: 12,
+                          ),
+                        ),
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            "Sign Up",
+                            style: TextStyle(color: buttonColor, fontSize: 12),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
